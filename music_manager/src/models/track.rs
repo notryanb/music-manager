9efcb65c7838734be24eb0c6a5a49c82074b6509
Ryan Blecher <notryanb@gmail.com>
2019-04-24 19:51:10 -0400

    create workspaces for cli and main manager

--- music_manager/src/models/track.rs
+++ music_manager/src/models/track.rs
@@ -4,12 +4,12 @@ use models::tag::Tag;
 use schema::*;
 
 pub struct Track {
-    pub File: File,
-    pub Tag: Tag,
+    pub file: File,
+    pub tag: Tag,
 }
 
 impl Track {
-    pub fn get_by_artist(artist: &str) -> Option<Track> {
+    pub fn get_by_artist(_artist: &str) -> Option<Track> {
         // use music_manager::schema::id3_tags::dsl::*;
         // id3_tags.inner_join(frames)
         None
